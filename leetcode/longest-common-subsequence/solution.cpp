00b296e49e77c664920bb12d55d631419f382454
Shreyas Raut <130643830+Rautshre@users.noreply.github.com>
2026-07-20 19:51:50 +0530

    feat: solve Longest Common Subsequence (LeetCode)

--- leetcode/longest-common-subsequence/solution.cpp
+++ leetcode/longest-common-subsequence/solution.cpp
@@ -15,13 +15,7 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         n1 = text1.size(), n2 = text2.size();
         vector<vector<int>>dp(n1+1, vector<int>(n2+1, 0));
-        // for(int i=0; i<=n1; i++){
-        //     dp[i][0] = 0;
-        // }
-        // for(int i=0; i<=n2; i++){
-        //     dp[0][i] = 0;
-        // }
-
+        
         for(int ind1 = 1; ind1<=n1; ind1++){
             for(int ind2 = 1; ind2<=n2; ind2++){
                 if(text1[ind1-1]==text2[ind2-1]){
